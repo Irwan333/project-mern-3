f6fc968b23bc242f0fd6b808b5fc129fefa0afbf
Irwan333 <irwandiwahyudi@gmail.com>
2020-07-08 00:23:20 +0700

    ecommerce client

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: "Any",
+    array: [],
+  },
+  {
+    _id: 1,
+    name: "Rp. 0 sampai Rp. 9.999",
+    array: [0, 9999],
+  },
+  {
+    _id: 2,
+    name: "Rp. 10.000 sampai Rp. 49.999",
+    array: [10000, 49999],
+  },
+  {
+    _id: 3,
+    name: "RP. 50.000 sampai Rp. 99.999",
+    array: [50000, 99999],
+  },
+  {
+    _id: 4,
+    name: "Rp. 100.000 sampai Rp. 500.000",
+    array: [100000, 500000],
+  },
+  {
+    _id: 5,
+    name: "Lebih dari RP. 500000",
+    array: [500001, 9999999],
+  },
+];
